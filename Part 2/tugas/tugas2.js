aecaff2ae7eda2396ce9032730af97bf10422357
Qxtlp <xi.tulop@yandex.com>
2022-03-15 15:25:16 +0700

    add input validation and explanation for used method

--- Part 2/tugas/tugas2.js
+++ Part 2/tugas/tugas2.js
@@ -6,28 +6,36 @@ const name = [
 ]
 
 function searchName(find, size, callback) {
-    let result = find
-    if (size > 0) {
-        result = callback(find);
-        if (result.length > size) {
-            size = result.length - size
-            return result.slice(0, -size);
+    if (typeof(find) == "string" && typeof(size) == "number" && typeof(callback) == "function") {
+
+        if (size > 0) {
+
+            result = callback(find);
+            if (result.length > size) {
+                size = result.length - size
+                return result.slice(0, -size); // menghapus index array sesuai ketentuan parameter
+            } else {
+                return result
+            }
+
         } else {
-            return result
+            return "mohon masukkan bilangan lebih dari 0"
         }
+
     } else {
-        return "mohon masukkan bilangan lebih dari 0"
+        return "pastikan input sudah benar"
     }
 }
 
 function filterName(input) {
     let result = [];
-    name.forEach(x => {
-        if (x.toLowerCase().includes(input.toLowerCase())) {
-            result.push(x);
+    name.forEach(x => { // mengekeskusi perintah yang di sediakan di parameter ke setiap index didalam array
+        //toLowerCase() digunakan untuk mengubah seluruh string menjadi huruf kecil
+        if (x.toLowerCase().includes(input.toLowerCase())) { //includes() digunakan untuk mengecek apakah variable memiliki value yang sama dengan parameter
+            result.push(x); //menambahkan value kebagian array yang paling akhir
         }
     });
     return result;
 }
 
-console.log(searchName("an", 4, filterName));
+console.log(searchName("an", 3, filterName));
