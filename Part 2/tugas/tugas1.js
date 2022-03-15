806e63b4efec77c58d2c3aa678b34b29be21ecbc
Qxtlp <xi.tulop@yandex.com>
2022-03-15 18:28:51 +0700

    change some example and explanation

--- Part 2/tugas/tugas1.js
+++ Part 2/tugas/tugas1.js
@@ -1,28 +1,31 @@
 // 10 Javascript Built-in function
 
-let a = " ini di ulang, ";
+let a = "ini di ulang,";
 
-//1. repeat() = return string yang menampilkan string yand disediakan sebanyak parameter yang ditentukan
+//1. repeat() = return string yang diulang sebanya parameter yang ditentukan
+
 console.log(a.repeat(3));
 
 //2. replace() = mengubah string yang sesuai dengan parameter pertama dan diubah menjadi parameter kedua
 
-console.log(a.replace("ulang", "ganti"));
+console.log(a.replace("ulang,", "ganti."));
 
-//3.slice() = menghapus perhuruf sebanyak parameter yang ditentukan
+//3. slice() = menghapus perhuruf / array sesuai parameter yang ditentukan
 
 console.log(a.slice(4));
+console.log([1, 2, 3, 4].slice(2));
 
-//4.Date() = return string tanggal dan waktu method di run 
+//4. Date() = return string tanggal dan waktu method di run 
+
 console.log(Date());
 
 
 //5. toExponential() = return number yang disingkat seperti yang ada di kalkulator saat panjang number melebihi digit yang ditentukan
 
-let b = 999999999999999999999999999999999999999999999999999999999999999999999999999999999
-console.log(b.toExponential(15));
+let b = 5555555555555555555555555555555555555555555555555555555555555555555555
+console.log(b.toExponential(10));
 
-//6.  Math.random() = return angka acak antara 0 dan 1 dan dengan sedikit rumus dapat return angka sesuai yang diinginkan
+//6.  Math.random() = return angka acak antara 0 dan 1 dan dengan sedikit rumus dapat return range angka yang diinginkan
 
 let c = Math.random() * 100
 console.log(c);
@@ -33,7 +36,7 @@ console.log(Math.round(c));
 
 //8. Math.sqrt() = return hasil akar kuadrat parameter
 
-console.log(Math.sqrt(121));
+console.log(Math.sqrt(25));
 
 //9. every() = mengecek value array sesuai dengan persyaratan yang ditentukan dan nantinya akan return boolean
 
@@ -50,5 +53,3 @@ Object.freeze(obj)
 obj.key = "value2"
 obj.prop = 3
 console.log(obj);
-
-//
